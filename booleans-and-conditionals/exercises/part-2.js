05761bcc8a293c8f6b482dcb58e56e0fdc65e9b1
byui-programmer-96 <lynnwriter96@gmail.com>
2024-06-20 14:01:40 -0500

    more changes

--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -46,3 +46,18 @@ if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
  }
 
 console.log(/* "Yes" or "No" */);
+
+if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
+    console.log("ENGINE FAILURE IMMINENT!");
+  } else if (fuelLevel <= 5000 || engineTemperature > 2500){
+    console.log("Check fuel level. Engines running hot.");
+  } else if (fuelLevel > 20000 && engineTemperature <= 2500){
+    console.log("Full tank. Engines good.");
+  } else if (fuelLevel > 10000 && engineTemperature <= 2500){
+    console.log("Fuel level above 50%. Engines good.");
+  } else if (fuelLevel > 5000 && engineTemperature <= 2500){
+    console.log("Fuel level above 25%. Engines good.");
+  } else {
+    console.log("Fuel and engine status pending...");
+  }
+
