ecce903492c826978df6540558865d1ef2f5f3e7
byui-programmer-96 <lynnwriter96@gmail.com>
2024-07-10 20:33:32 -0500

    done

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,5 +1,19 @@
 //Create an anonymous function and set it equal to a variable.
+let practice = function(myArg) {
+    if (typeof myArg === "number") {
+       return myArg * 3;
+    } else if (typeof myArg === "string") {
+       return "ARRR!";
+    } else {
+       return myArg;
+    }
+ 
+ }
 
+let irs = function(levelOfFuel, itemsInCargo) {
+    let arr = deckMops(itemsInCargo);
+    return `Raided ${nonSuspiciousFunction(fuelLevel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
+ }
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
@@ -11,5 +25,47 @@ a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
+
+let nonSuspiciousFunction = function(a) {
+    if (checkFuel(a) === 'green') {
+       return a - 100001;
+    }
+    else if (checkFuel(a) === 'yellow') {
+       return a - 50001;
+    }
+    else {
+       return a;
+    }
+ };
+
+
+
+function checkFuel(level) {
+    if (level > 100000){
+       return 'green';
+    } else if (level > 50000){
+       return 'yellow';
+    } else {
+       return 'red';
+    }
+ }
+ 
+ function holdStatus(arr){
+    if (arr.length < 7) {
+       return `Spaces available: ${7 - arr.length}`;
+    } else if (arr.length > 7){
+       return `Over capacity by ${arr.length - 7} items.`
+    } else {
+       return "Full";
+    }
+ }
+ 
+ let fuelLevel = 200000;
+ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+ 
+ console.log("Fuel level: "+ checkFuel(fuelLevel));
+ console.log("Hold status: "+ holdStatus(cargoHold));
+
+
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
