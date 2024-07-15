d78ce9cfb1a8206a81bdcc88d0c3071dc46b62e8
byui-programmer-96 <bam21002@byui.edu>
2024-07-15 17:43:58 -0500

    1st save

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -18,3 +18,17 @@ function whoWon(player1,player2){
  
    return 'Player 1 wins!';
  }
+const test = require('../RPS.js');
+
+module.exports = {
+  whoWon: whoWon
+};
+test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
+  let output = test.whoWon('rock','paper');
+  expect(output).toBe("Player 2 wins!");
+});
+
+test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
+  let output = test.whoWon('paper','scissors');
+  expect(output).toBe("Player 2 wins!");
+});
