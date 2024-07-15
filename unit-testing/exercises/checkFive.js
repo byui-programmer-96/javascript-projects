d231f3a5b299223b3e24136b30a86a0174993836
byui-programmer-96 <bam21002@byui.edu>
2024-07-15 17:22:02 -0500

    ..

--- unit-testing/exercises/checkFive.js
+++ unit-testing/exercises/checkFive.js
@@ -9,3 +9,14 @@ function checkFive(num){
    }
    return result;
  }
+ const test = require('../checkFive.js');
+ const checkFive = require('../checkFive.js');
+
+ describe("checkFive", function(){
+
+    test("returns 'num is less than 5' when num < 5.", function(){
+       let output = checkFive(2);
+       expect(output).toBe("2 is less than 5.");
+    });
+
+ });
