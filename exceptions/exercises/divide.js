dd6e2a717be052730dd5eec4c160df8da9a45764
byui-programmer-96 <bam21002@byui.edu>
2024-07-23 17:28:14 -0500

    complete

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,16 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+let numerator = 0
+let denominator = 0
+function divide(numerator, denominator){
+    if (denominator === 0){
+        throw Error("Attempted to divide by zero.");
+    } else {
+        let result = numerator/denominator;
+    return result;
+    }
+};
+
+console.log(divide(8,0));
